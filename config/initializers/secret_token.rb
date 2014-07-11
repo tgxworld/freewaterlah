1692664d4c6a1491a87bd6af997645d9aa8b5a7e
Guo Xiang Tan <tgx_world@hotmail.com>
2014-07-11 17:23:39 +0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freewaterlah::Application.config.secret_key_base = '760b8cd410008d7cb7935901f9a98173adac053463d96d13bb4f3470118c8a3f1ad006b1dd37fc9ac2f9cbf45d6a85d2f4e2c39f7fc7f6f83be25cae778588b2'
