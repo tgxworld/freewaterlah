ab7145473d05ad32ebbb8b81e1c4e11d52febfa0
Guo Xiang Tan <tgx_world@hotmail.com>
2014-07-14 19:58:31 +0800

    Setup HomepageController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Freewaterlah::Application.routes.draw do
+  root to: 'homepage#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
