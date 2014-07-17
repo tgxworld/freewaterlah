c8fb0fb137a99a87b3aed4b97f4808bb95fc5f6a
Guo Xiang Tan <tgx_world@hotmail.com>
2014-07-17 20:33:18 +0800

    Alot of changes.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def credits
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Freewaterlah::Application.routes.draw do
   root to: 'homepage#index'
 
   resources :restaurants, only: [:index, :new, :create, :edit, :update]
+
+  get '/credits' => 'static_pages#credits'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
